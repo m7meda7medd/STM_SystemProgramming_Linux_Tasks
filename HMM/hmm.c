633d99d40ad24986d877acb6e57752b0f735faf3
m7meda7medd <moahmed1907@gmail.com>
2024-04-17 17:35:49 -0500

    Heap Memory manager added after testing and running bash on it

--- HMM/hmm.c
+++ HMM/hmm.c
@@ -1,128 +1,347 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+
 #include "hmm.h"
 // my free list 
 node_t * head  = NULL ;
 // traverse free list function
-unsigned char init_freelist (void* start_pb,size_t needed_size) {
+int init_freelist (void) {
 if (NULL == head) {
-  head = start_pb ;
-  head->block_size = SBRK_INCREMENT - sizeof(node_t);
+  head =  (node_t*) sbrk(SBRK_INCREMENT);
+  if (head == (void*)-1){
+    return -1 ;
+  }else{
+  head->block_size = SBRK_INCREMENT - sizeof(size_t);
   head->next = NULL ; 
-  head->prev = NULL ; 
-  head->block_state = FREE ; 
+  head->prev = NULL ;
   return 0  ;
+} 
 }
-return 1 ; 
+else{
+ return -2 ;
+}
 }
 
 void* split(node_t* node,size_t needed_size){
-      // i have node that have block size larger than needed_size 
-      size_t reminder_size ; 
-      reminder_size = node->block_size - needed_size ; 
-  if (reminder_size < MIN_FREE_BLOCK_SIZE ){
-      // traverse for another node
-      return NULL ; 
- }else{
-      // make new free node 
-      node->next =  (node_t*)((unsigned char*) node + node->block_size) ;
-      node->next->block_size = reminder_size - sizeof(node_t) ;
-      node->next->prev = node ; 
-      node->next->next = NULL ; 
-      node->next->block_state = FREE ;   
-      // point to next free block     
-      node->block_state = ALLOCATED ;
-      return (void*) node ;   
- } 
-}	
-node_t* traverse_freelist (node_t* node , size_t needed_size) {
- if ( NULL == node ) {
-  node_t* new_node = (node_t*) sbrk(SBRK_INCREMENT) ;
-      if (node == (node_t*) -1)
-      {
-        perror("Error with SBRK") ; 
-        exit(2) ;
-      }
-      else{
-        node_t* last_node = head ; 
-        while (last_node->next != NULL){
-          last_node = last_node->next ; 
-        }
-        if (last_node->block_state == FREE){
-          last_node->block_size+=SBRK_INCREMENT ;
-          return last_node ; 
-        }else {
-          new_node->block_size=SBRK_INCREMENT - sizeof(node_t); 
-          new_node->prev = last_node ; 
-          new_node->block_state = FREE ; 
-          new_node->next = NULL ; 
-          last_node->next = new_node ;
-        }
-        
-      }
 
+  if (node->prev == NULL){              
+        //split head      
+        node_t* new_node = (node_t*)( ((unsigned char*) node + needed_size + sizeof(size_t))) ;
+        new_node->block_size = node->block_size - (needed_size + sizeof(size_t)) ;
+        node->block_size = needed_size ; // set the size of allocated block
+        if (head->next == NULL)
+        {
+          new_node->next = NULL ; 
+        }
+        else{
+          new_node->next = head->next ;
+        }
+        head = new_node ;
+        new_node->prev = NULL ;                
+      return (void*) node ;
+  }
+  else if (node->next == NULL)
+  {
+        node_t* new_node = (node_t*)((unsigned char*) node + needed_size + sizeof(size_t)) ; //shift the pointer 
+        // set the sizes of new_node and node
+        new_node->block_size = node->block_size - (needed_size + sizeof(size_t)) ;
+        node->block_size = needed_size ; // set the size of allocated block
+        //make the next of new_node to NULL 
+        new_node->next = NULL ;
+        // make the prev of node points to the new node 
+        node->prev->next = new_node ;                
+      return (node_t*) node ; // return the pointer of allocated 
+
+  }else{
+        node_t* new_node = (node_t*) ((unsigned char*) node + needed_size + sizeof(size_t)) ; //shift the pointer 
+        // set the sizes of new_node and node
+        new_node->block_size = node->block_size - (needed_size + sizeof(size_t)) ;
+        node->block_size = needed_size ; // set the size of allocated block
+        
+        new_node->next = node->next ; // make the next of new_node to the next of node 
+        node->prev->next = new_node ; // make the prev of node points to the new node  
+                      
+      return (node_t*) node  ; // return the pointer of allocated 
+  }
+}	
+
+void* traverse_freelist (node_t* node , size_t needed_size) {
+ if (needed_size == node->block_size){
+  if (node->prev == NULL){              
+                                        
+    head = node->next ;               
+                                        
+     if (head != NULL){                                 
+      head->prev = NULL ; 
+     }
+     return (node_t*) node ;
+  }
+  else if (node->next == NULL)
+  {
+    node->prev->next = NULL ; 
+    
+    return (node_t*) node  ;
+
+  }else{
+    node->prev->next = node->next ;
+    node->next->prev = node->prev ; 
+
+    return (node_t*)  node ;
+
+  }
  }
-  if (node->block_state == FREE){
-    if (node->block_size == needed_size){
-        node->block_state = ALLOCATED ;
-        return node ;  
-      }
-    else if (node->block_size > needed_size){
-      // split 
-      void* ret_ptr = NULL ; 
-      ret_ptr =  split(node,needed_size) ; 
-      if (NULL == ret_ptr)
-      {
-        traverse_freelist(node->next, needed_size) ;
+ else if (node->block_size > needed_size) {
+  if ((node->block_size - needed_size) > MIN_FREE_BLOCK_SIZE){
+
+    return split(node,needed_size) ;
+  }
+ else{
+  if (node->next == NULL) //found that the next equals NULL 
+  {
+     node_t* temp_node = (node_t*) sbrk(SBRK_INCREMENT) ;//move the SBRK 
+      if ( (void *) temp_node == (void*) -1 ){
+          return NULL ; 
       }
       else{
-        return ret_ptr ;  
+        // adjust the metadata of new node 
+        temp_node->block_size= SBRK_INCREMENT - sizeof(size_t) ;
+        if ((node_t*)((unsigned char*)node+(node->block_size + sizeof(size_t))) == temp_node){
+          //merge them 
+          node->block_size +=temp_node->block_size + sizeof(size_t) ;
+          return traverse_freelist(node,needed_size) ;  //traverse the freelist 
+        }else{
+        node->next = temp_node ;  // add the new_node to the free list
+        temp_node->prev = node ; 
+        temp_node->next = NULL ;
+        return traverse_freelist(node->next,needed_size) ;  //traverse the freelist 
+        }
+      }     
+  }
+  else{
+    return traverse_freelist(node->next,needed_size) ;
+  }
+  
+ }
+ }
+ else{
+  if (node->next == NULL) //found that the next equals NULL 
+  {
+      node_t* temp_node = (node_t*) sbrk(SBRK_INCREMENT) ;//move the SBRK 
+      if ( (void *) temp_node == (void*) -1 ){
+          return NULL ; 
       }
+      else{
+        // adjust the metadata of new node 
+        temp_node->block_size= SBRK_INCREMENT - sizeof(size_t) ;
+        if ((node_t*)((unsigned char*)node+(node->block_size + sizeof(size_t))) == temp_node){
+          //merge them 
+          node->block_size +=temp_node->block_size + sizeof(size_t) ;
+          return traverse_freelist(node,needed_size) ;  //traverse the freelist 
+        }else{
+        node->next = temp_node ;  // add the new_node to the free list
+        temp_node->prev = node ; 
+        temp_node->next = NULL ;
+        return traverse_freelist(node->next,needed_size) ;  //traverse the freelist 
+        }
+
+         
+      }   
+  }
+  else{
+    return traverse_freelist(node->next,needed_size) ;
+  }
+ }
+}
+
+
+
+void* HMM_malloc(size_t size ){
+ if (size < MIN_FREE_BLOCK_SIZE){
+  size = MIN_ALLOCATE ; 
+ }
+node_t* node_ptr = NULL ; 
+if (size == 0 ){
+  return NULL ; 
+}
+if (head == NULL)
+{
+    if (!init_freelist()){
+      node_ptr = traverse_freelist(head,size) ;
+
+    }else{
+      // error can't init free_list
+      node_ptr =  NULL ; 
     }
-    else{ 
-      traverse_freelist(node->next , needed_size) ; 
-    }
-  } 
-  else { 
-    traverse_freelist(node->next , needed_size) ; 
+}else {
+  // there's a free list 
+  node_ptr = traverse_freelist(head,size) ; 
+}
+return (void*)((unsigned char*)node_ptr + (sizeof(size_t))) ;
+} 
+
+/*
+*
+* merge : merge two free nodes 
+*
+*
+*/
+void merge(node_t* left_node, node_t* right_node) {
+
+  if ((node_t*) ((unsigned char*)left_node+(left_node->block_size)+sizeof(size_t)) == (node_t*)  right_node ){
+    
+    left_node->block_size+= (right_node -> block_size +(sizeof(size_t))) ;
+    left_node->next = right_node->next ;
   }
 }
 
-void* HMM_malloc(size_t size ){
-  size+=sizeof(size_t) ;
-if (NULL == head) {
-  void* start =  sbrk(SBRK_INCREMENT) ;
-  if (start == (void*) -1 ){
-    perror("Error with SBRK\n") ;
-    exit(1)  ;
+void check_on_tail_size(node_t* tail){
+ void* current_pb = sbrk(0) ;
+ void* shifted_tail = (void*) ((unsigned char*)tail+(tail->block_size)+sizeof(size_t)) ; 
+ if (tail->block_size >= (SBRK_INCREMENT- sizeof(size_t)))
+{ 
+ if (shifted_tail ==  current_pb){
+  if (tail->prev != NULL){
+    tail->prev->next = NULL ; 
+  }else{
+    head = NULL ; 
+  }
+  
+  void* ret_ptr = sbrk(-(tail->block_size+(sizeof(size_t)))) ; 
+  if (ret_ptr == (void*) -1){
+    return ; 
+  }
+ }
+}
+}
+
+
+
+void put_node_at_head (node_t* node){ 
+  node->next = head ; 
+  if (head != NULL) {
+  head->prev = node ;
   } 
-  else{
-    if (!init_freelist(start,size)) {
-      perror("Error to init free_list\n") ; 
-      exit(1) ;
-      }   
-    else {
-      node_t* ret_ptr = traverse_freelist(head,size) ;
-
-      } 
+  node->prev = NULL  ; 
+  head = node ; 
 }
+void put_node_between_two_nodes (node_t* node,node_t* next_node){
+// bsm ellah el rahman el rahem
+node->prev = next_node->prev ; 
+node->next = next_node ; 
+next_node->prev->next = node ; 
 }
-return NULL ; 
+void put_node_at_tail(node_t* node, node_t* tail){
+tail->next = node ; 
+node->next = NULL ; 
+node->prev = tail->prev ; 
 }
 
-int main ()
-{
-  /*
-void* ptr1 = NULL ;
-void* ptr2 = NULL ;  
-ptr1 = HMM_malloc(64) ; 
-if (ptr1 == NULL){
-while(1) ; 
-}
-*/
-return 0 ;
+void HMM_free(void* ptr){
+   char ret = 0 ;
+  if (ptr == NULL)
+  {
+    return ; 
+  }
+
+  node_t* param_node = (node_t*) ((unsigned char*) ptr - (sizeof(size_t))) ;
+  if (param_node == NULL){
+    return ; 
+  } 
+  node_t* temp_node = head ; 
+  if (temp_node == NULL)
+  {
+          put_node_at_head(param_node) ;  
+          //merg with the next one 
+          merge(param_node,param_node->next) ;
+          if (param_node->next == NULL){
+             check_on_tail_size(param_node) ;
+          }
+  }else{
+  while (temp_node)
+  { 
+    if (temp_node == param_node){
+      break ; 
+    }
+    else if (temp_node > param_node){
+      if (temp_node == head){
+          // add node to head 
+          put_node_at_head(param_node) ;  
+          //merg with the next one 
+          merge(param_node,param_node->next) ;
+          if (param_node->next == NULL){
+             check_on_tail_size(param_node) ;
+          }
+          break ; 
+      }else{
+        put_node_between_two_nodes(param_node,temp_node) ;//add at the mid of nodes
+        merge(param_node,temp_node) ;  //merg with the previous and then merg with the next
+        node_t* dummy = param_node ; 
+        merge(param_node->prev,param_node) ; //merge with the next
+        if (param_node->prev != NULL){
+        if (param_node->prev->next == NULL){
+            check_on_tail_size(param_node->prev) ;
+        }
+        }
+        break ;    
+      }
+    }else{
+      if (temp_node->next == NULL)
+      {
+        // add at the end of free list 
+        put_node_at_tail(param_node,temp_node) ;
+        // merge the last node with the previous one
+        merge(temp_node,param_node);
+        // check if the size of last node 
+         if (temp_node->next == NULL){
+          //the temp_node and the param node merged
+          check_on_tail_size(temp_node) ; 
+         }else{
+          //not merged 
+          check_on_tail_size(temp_node->next) ;
+         }
+         break ;  
+      }
+    }
+
+    temp_node = temp_node->next ; 
+  }
+  }
+  }
+
+  void* HMM_calloc(size_t nmemb, size_t size){
+    size_t total_size = (size*nmemb) ;
+    void* ptr = HMM_malloc(total_size) ; // allocate
+    memset(ptr,0,total_size) ;
+    return ptr ; 
+  }
+
+  void* HMM_realloc(void* old_ptr, size_t new_size){
+    void* new_ptr = NULL ;
+    size_t old_size = *(size_t*)((unsigned char*)old_ptr-sizeof(size_t)) ;
+    new_ptr = HMM_malloc(new_size) ; // allocate the new_size
+
+    memcpy(new_ptr,old_ptr,old_size);
+
+    HMM_free(old_ptr) ;
+    return new_ptr ; 
+  }
 
 
+/*
+ int main ()
+ {
+  void* current_pb = sbrk(0) ; // 0x555555559000
+   void* ptr5 = HMM_malloc(4*4096) ;
+    void* ptr6 = HMM_malloc(4*4096) ;
+    current_pb = sbrk(0) ; // 0x555555562000
+        HMM_free(ptr6) ;
+        current_pb = sbrk(0) ;//;0x55555555d008
+        HMM_free(ptr5) ;
+  current_pb = sbrk(0) ;
+ while (1) ;
 
-}
+ }
+ */
+/*
+ pb 
+ 0x555555559000
+ 0x5555555d7000
+ */
--- HMM/hmm.h
+++ HMM/hmm.h
@@ -1,20 +1,28 @@
 #ifndef HMM_H
 	#define HMM_H
 
-#define MIN_ALLOCATE 64
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MIN_ALLOCATE 16
 #define PAGE_SIZE  4096
 #define SBRK_INCREMENT    (unsigned long)(3*PAGE_SIZE)
 #define MIN_FREE_BLOCK_SIZE (sizeof(node_t)+MIN_ALLOCATE)
-// block states  
-#define ALLOCATED 1 
-#define FREE  0 
 
-typedef struct node {
-struct node* next ; 
-struct node* prev ; 
-size_t block_size  ;
-size_t block_state ;
+typedef struct node { 
+size_t block_size ;
+struct node* next 	; 
+struct node* prev 	;
 }node_t ;
+
+
+void* HMM_malloc(size_t size );
+void HMM_free(void* ptr) ;
+void* HMM_realloc(void* old_ptr, size_t new_size) ;
+void* HMM_calloc(size_t nmemb, size_t size) ;
+
+
 
 
 

73ddb3092b14d6e17f202709e7fa21f6b5369471
m7meda7medd <moahmed1907@gmail.com>
2024-04-20 22:31:46 -0500

    HMM tested with many scripts on many test cases and tested with bash and many external commands

--- HMM/hmm.h
+++ HMM/hmm.h
@@ -10,7 +10,7 @@
 #define PAGE_SIZE  4096
 #define SBRK_INCREMENT    (32*PAGE_SIZE)
 #define SBRK_DECREMENT    (28*PAGE_SIZE)
-#define MIN_FREE_BLOCK_SIZE (sizeof(node_t)+MIN_ALLOCATE)
+#define MIN_FREE_BLOCK_SIZE (sizeof(size_t)+MIN_ALLOCATE)
 
 typedef struct node { 
 size_t block_size ;
--- HMM/test.c
+++ HMM/test.c
@@ -1,6 +1,28 @@
 
-#include "hmm.h"
-// my free list 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define NUM_ALLOCS 10000
+#define MAX_SIZE 100
+#define MAX_ITERATIONS 10000
+
+
+#define MIN_ALLOCATE 16
+#define PAGE_SIZE  4096
+#define SBRK_INCREMENT    (32*PAGE_SIZE)
+#define SBRK_DECREMENT    (28*PAGE_SIZE)
+#define MIN_FREE_BLOCK_SIZE (sizeof(size_t)+MIN_ALLOCATE)
+
+typedef struct node { 
+size_t block_size ;					
+struct node* next 	; 
+struct node* prev 	;
+}node_t ;
+
+
 
 // my free list 
 static node_t *head = NULL;
@@ -400,3 +422,43 @@ void *realloc(void *old_ptr, size_t new_size)
 }
 
 
+void random_alloc_free_test() {
+    srand((unsigned int)time(NULL));
+    
+    void* pointers[NUM_ALLOCS] = {NULL};
+    
+    for (int i = 0; i < MAX_ITERATIONS; ++i) {
+        int index = rand() % NUM_ALLOCS;
+        if (pointers[index] == NULL) {
+            // Allocate memory
+            size_t size = (size_t)(rand() % MAX_SIZE) + 1;
+            pointers[index] = malloc(size);
+            if (pointers[index] != NULL) {
+                printf("Allocated memory of size %zu at address %p\n", size, pointers[index]);
+            } else {
+                fprintf(stderr, "Allocation failed for size %zu\n", size);
+            }
+        } else {
+            // Free memory
+            printf("Freeing memory at address %p\n", pointers[index]);
+            free(pointers[index]);
+            pointers[index] = NULL;
+        }
+    }
+    
+    // Free remaining allocated memory
+    for (int i = 0; i < NUM_ALLOCS; ++i) {
+        if (pointers[i] != NULL) {
+            printf("Freeing remaining memory at address %p\n", pointers[i]);
+            free(pointers[i]);
+            pointers[i] = NULL;
+        }
+    }
+}
+
+int main() {
+    printf("Starting random allocation and deallocation test...\n");
+    random_alloc_free_test();
+    printf("Test complete.\n");
+    return 0;
+}
